b0f145af24fe6ffe505c4fbe7f1d19c52bfec112
threelittlelees1-cloud <threelittlelees1@gmail.com>
2026-04-15 17:09:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const request = require("request");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
 
-app.use("/", (req, res) => {
-  req.pipe(
-    request("https://eaglercraft-xwdtvpgm.usw-1.sealos.app")
-  ).pipe(res);
-});
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "https://eaglercraft-xwdtvpgm.usw-1.sealos.app",
+    changeOrigin: true,
+    ws: true,
+    secure: true
+  })
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
